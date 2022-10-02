edd13580170715ab8ed6868ee7b6aae4af91b1ff
me <me@robertcurtiss.com>
2022-10-02 10:46:24 -0400

    Added instance names to the resistor class r1 = Resistor(100) print(r1.symbol) r1

--- Chapter-6/resistor.py
+++ Chapter-6/resistor.py
@@ -2,9 +2,84 @@
 
 """
 # - Contains functions to calculate series and parallel voltages, current and total resistance
-# - serve this page by using python -m mkdocs server from the command line
+# - serve this page by using python -m mkdocs serve from the command line
 
 """
+from dataclasses import fields
+import traceback
+
+
+class Resistor:
+    """Attributes of one resistor
+    """
+
+    def __init__(self, ohms: [int, float]):
+        """
+        Parameters:
+            ohms[int | float]: Value of the resistor in ohms
+            symbol(string): Name of the resistor
+
+        Examples:
+        >>> r1 = Resistor(ohms=100)
+
+        >>> r1.current(25)
+        0.25
+
+        >>> r1.voltage_drop(1000,10)
+        1.0
+
+        >>> r1.power(10)
+        1.0
+
+        >>> r1.ohms
+        100
+    """
+
+        self._ohms = ohms
+        self._volts = 10
+        (filename, line_number, function_name, text) = traceback.extract_stack()[-2]
+        self._def_name = text[:text.find('=')].strip()
+
+    @property
+    def symbol(self):
+        return self._def_name
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    def current(self, voltage):
+        return round(float(voltage / self._ohms), 2)
+
+    @property
+    def volts(self, volts):
+        self._volts = volts
+
+    @volts.setter
+    def volts(self, value):
+        self._volts = value
+
+    def voltage_drop(self, total_resistance, voltage):
+        """
+        # Voltage drop equals the resistance divided by the  total resistance multiplied by the voltage
+        - vd = r/rt * v
+        :param total_resistance:
+        :param voltage:
+        :return:
+        """
+        return float((self._ohms / total_resistance) * voltage)
+
+    def power(self, voltage):
+        """
+        ## Power equals current squared times resistance
+        - P = I * I * R
+        Examples:
+            >>> Ri = Resistor(100)
+
+            >>> Ri.ohms
+            100
+        """
+        return round(float(self.current(voltage) ** 2 * self._ohms), 2)
 
 
 class Series:
@@ -12,48 +87,39 @@ class Series:
     ## Passed a list of resistor values, and voltages, it will calculate voltage drop across each resistor , total resistance and total current
     """
 
-    def __init__(self, resistors: list[float, int], vs: [int, float]):
+    def __init__(self, resistors: list[Resistor], vs: [int, float]):
         """
         - resistors is a list of resistors in ohms
         - vs is the voltage source in volts
         Examples:
-            >>> x = Series([10,10],10)
-            >>> x.volts # volts
+            >>> r10 = Resistor(10)
+            >>> s2 = Series([r10,r10],10)
+            >>> s2.volts # volts
             10
-            >>> x.it
-            0.5
-            >>> x.rt
-            20
-            >>> r1 = Series([100,100],10)
-            >>> r2 = Series([200,200],vs=20)
-            >>> r3 = Series([r1.rt,r2.rt], 25)
-            >>> r3.rt
-            600
 
         param resistors:list[int,float]
         param volts:[int,float]
         """
         self._resistors = resistors
-        if isinstance(self._resistors[0], Series):
-            print('Yes')
-            self._resistors[0] = self._resistors[0].rt
         self._volts = vs
+        (filename, line_number, function_name, text) = traceback.extract_stack()[-2]
+        self._def_name = text[:text.find('=')].strip()
 
     @property
     def rt(self):
         """
         rt = r1 + r2 + r3 + r...
         Examples:
-            >>> x =Series([33, 68, 100, 47, 10], 10)
-            >>> x.rt
-            258
+            >>> r33 = Resistor(33)
+            >>> r68 = Resistor(68)
+            >>> s3 =Series([r33, r68], 10)
+            >>> s3.rt
+            101
 
         return: Total Ohms in the series circuit
         """
-        if isinstance(self._resistors[0], Series):
-            print(f'{self._resistors[0].rt}')
         rt = 0
-        [rt := rt + r for r in self._resistors]
+        [rt := rt + r.ohms for r in self._resistors]
         return rt
 
     @property
@@ -61,9 +127,10 @@ class Series:
         """
         it = volts/rt
         Examples:
-            >>> x = Series([82,18,15,10], vs=25)
-            >>> x.it
-            0.2
+            >>> r100 = Resistor(100)
+            >>> s4 = Series([r100,r100], 10)
+            >>> s4.it
+            0.05
 
         return: Total Current in the series circuit
         """
@@ -73,18 +140,22 @@ class Series:
     @property
     def voltage_drops(self):
         """
-        voltage_drop = restance/rt * volts
+        voltage_drop = resistance/rt * volts
         Examples:
-            >>> x = Series([100,100], 10)
-            >>> x.voltage_drops
-            {'R1=100Ω': 5.0, 'R2=100Ω': 5.0}
+            >>> R100 = Resistor(100)
+            >>> R200 = Resistor(200)
+            >>> s5 = Series([R100,R200],100)
+            >>> s5.voltage_drops
+            {'R100=100Ω': 33.33, 'R200=200Ω': 66.67}
+
+        {'R1=100Ω': 5.0, 'R2=100Ω': 5.0}
 
         return: Voltage drop dictionary, for each resitor in the circuit
         """
         drops = dict()
         n = 1
         for r in self._resistors:
-            drops[f'R{n}={r}\u03A9'] = round((r / self.rt) * self._volts, 2)
+            drops[f'{r.symbol.upper()}={r.ohms}\u03A9'] = round((r.ohms / self.rt) * self.volts, 2)
             n += 1
         return drops
 
@@ -92,12 +163,6 @@ class Series:
     def volts(self):
         """
         Examples:
-            >>> v = Series([100,200], 10)
-            >>> v.volts
-            10
-            >>> v.volts = 26.6
-            >>> v.volts
-            26.6
 
         :return:
         """
@@ -108,14 +173,38 @@ class Series:
         self._volts = value
 
     def __repr__(self):
+
         return f"rt={self.rt} \u03A9" \
-               f"\nit={self.it}" \
-               f"\nvolts({self._volts})={self.voltage_drops}"
+               f"\nvolts({self._volts})={self.voltage_drops}" \
+               f"\nit={self.it}"
+
+class Parallel:
+    """Class will calculate current, total ohms for parallel resistors"""
+
+    def __init__(self, resistors: list[Resistor], volts: [int | float] = 1):
+        self._resistors = resistors
+        self._volts = volts
+
+        for r in self._resistors:
+            r.volts = self._volts
+
+    def rt(self) -> float:
+        total = 0
+        [total := total + r.ohms ** -1 for r in self._resistors]
+        return round(float(total ** -1), 2)
 
 
-x = Series([33, 68, 100, 47, 10], 10)
+r33 = Resistor(33)
+r68 = Resistor(68)
+r3 = Resistor(100)
+r4 = Resistor(47)
+r5 = Resistor(10)
+x = Series([r33, r68, r3, r4, r5], 10)
 print(x)
-y = Series([x], 35)
-print(y)
-x.volts = 25
-print(x)
+
+r6 = Resistor(100)
+s1 = Series([r6, r6], 10)
+print(s1)
+
+p1 = Parallel([r3, r3])
+print(p1.rt())
